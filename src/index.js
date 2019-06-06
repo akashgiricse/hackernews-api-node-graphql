bea483414661b11c699c0601d99835742e9301de
akashgiricse <akashgiricse@gmail.com>
2019-06-06 12:43:31 +0530

    Define schema in its own file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-
-	type Query {
-		info: String!
-		feed: [Link!]!
-	}
-
-	type Link {
-		id: ID!
-		description: String!
-		url: String!
-	}
-`
-
-
 let links = [
 	{
 		id: 'link-0',
@@ -44,7 +28,7 @@ const resolvers = {
 
 const server = new GraphQLServer({
 
-	typeDefs,
+	typeDefs: './src/schema.graphql',
 	resolvers,
 })
 
