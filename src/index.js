8666b9a44ec460f2f2af37e7425f96ac28bfcc1f
akashgiricse <akashgiricse@gmail.com>
2019-06-06 17:00:27 +0530

    Add prisma for storing data

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
+const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 
-let links = [
-	{
-		id: 'link-0',
-		url: 'www.howtographql.com',
-		description: 'Fullstack tutorial for GraphQL',
-		
-	}
-]
 
-// 2
 const resolvers = {
 	Query: {
 		info: () => 'This is the API of hackernews clone',
 		//2 
-		feed: () => links,
+		feed: (root, args, context, info) =>{
+			return context.prisma.links()
+		},
 	},
 
-	Link: {
-		id: (parent) => parent.id,
-		description: (parent) => parent.description,
-		url: (parent) => parent.url,
-	}
+	Mutation: {
+		post: (root, args, context) => {
+			return context.prisma.createLink({
+				url: args.url,
+				description: args.description,
+			})
+		},
+	},
 }
-
-// 3
 
 const server = new GraphQLServer({
 
 	typeDefs: './src/schema.graphql',
 	resolvers,
+	context: { prisma },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
